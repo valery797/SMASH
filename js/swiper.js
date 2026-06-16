f7c536f0412d613e1355f1d0474131da41e0e906
valery797 <rudakova2002@icloud.com>
2026-06-16 17:58:46 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper-merch", {
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
